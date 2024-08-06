7b25c5a5cac91fc38ebc084b53d963c888eb1366
Vinay Kamuju <vinaykamuju@gmail.com>
2024-08-06 19:03:31 +0200

    Add frontend folder

---
+++ frontend/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import App from './App';
+import AboutUs from './components/AboutUs';
+import './index.css';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<AboutUs />} />
+    </Routes>
+  </Router>
+);
